4e4912c845d9c5f06539526af0608ffb2b4f308b
Torchane djihane <121492777+djihane19@users.noreply.github.com>
2025-09-05 16:52:28 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,5 +6,5 @@ module.exports = greet; //Ensure function is exportable
 //if run directly, print output (useful for debugging)
 
 if (require.main= module){
-    console.log('Worldd')
+    console.log('World')
 }
